a91128484bdcb6f73a488cc6d59407cf76be7d13
Igor Barreto <igorbarreto1993@hotmail.com>
2020-10-09 19:05:15 -0300

    adicionado password utils

---
+++ src/main/java/br/com/igbarreto/pontointeligente/api/utils/PasswordUtils.java
@@ -0,0 +1,23 @@
+package br.com.igbarreto.pontointeligente.api.utils;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class PasswordUtils {
+
+	private static final Logger log = LoggerFactory.getLogger(PasswordUtils.class);
+
+    public PasswordUtils() {}
+	
+	public static String gerarBCrypt(String senha) {
+		if(senha ==null) {
+			return senha;
+		}
+		log.info("Gerando senha com bCrypt");
+		BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+		return bCryptPasswordEncoder.encode(senha);
+		}
+	}
+
